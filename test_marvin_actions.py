ce029956a0fafd2f05d7a5e039903a57d8346b8f
Daniel Persson <daniel@silvertejp.org>
2024-08-24 18:09:02 +0200

    Add unittest for Source action

--- test_marvin_actions.py
+++ test_marvin_actions.py
@@ -62,3 +62,9 @@ class ActionTest(unittest.TestCase):
 
         self.assertActionSilent(marvin_actions.marvinExplainShell, "explains")
 
+    def testSource(self):
+        """Test that marvin responds to questions about source code"""
+        self.assertStringsOutput(marvin_actions.marvinSource, "source", "source")
+        self.assertStringsOutput(marvin_actions.marvinSource, "källkod", "source")
+        self.assertActionSilent(marvin_actions.marvinSource, "opensource")
+
