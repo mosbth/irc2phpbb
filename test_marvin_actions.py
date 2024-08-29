fc7386fd679ef57198187c0fa5f11ea146f310e3
Daniel Persson <daniel@silvertejp.org>
2024-08-29 17:58:37 +0200

    Add unittest for TimeToBBQ action

--- test_marvin_actions.py
+++ test_marvin_actions.py
@@ -9,6 +9,7 @@ import json
 import re
 import unittest
 
+from datetime import date
 from unittest import mock
 
 import marvin_actions
@@ -46,6 +47,23 @@ class ActionTest(unittest.TestCase):
             else:
                 expectedOutput = expectedOutput.get(subkey)
         self.assertActionOutput(action, message, expectedOutput)
+
+
+    def assertBBQResponse(self, todaysDate, bbqDate, expectedMessageKey):
+        """Assert that the proper bbq message is returned, given a date"""
+        url = self.strings.get("barbecue").get("url")
+        message = self.strings.get("barbecue").get(expectedMessageKey)
+        if isinstance(message, list):
+            message = message[1]
+        if expectedMessageKey in ["base", "week", "eternity"]:
+            message = message % bbqDate
+
+        with mock.patch("marvin_actions.datetime") as d:
+            d.date.today.return_value = todaysDate
+            with mock.patch("marvin_actions.random") as r:
+                r.randint.return_value = 1
+                expected = f"{url}. {message}"
+                self.assertActionOutput(marvin_actions.marvinTimeToBBQ, "dags att grilla", expected)
 
 
     def testWhois(self):
@@ -156,3 +174,16 @@ class ActionTest(unittest.TestCase):
         with mock.patch("marvin_actions.random") as r:
             r.randint.return_value = 123
             self.assertActionOutput(marvin_actions.marvinStrip, "random strip kanske?", expected)
+
+    def testTimeToBBQ(self):
+        """Test that marvin knows when the next BBQ is"""
+        self.assertBBQResponse(date(2024, 5, 17), date(2024, 5, 17), "today")
+        self.assertBBQResponse(date(2024, 5, 16), date(2024, 5, 17), "tomorrow")
+        self.assertBBQResponse(date(2024, 5, 10), date(2024, 5, 17), "week")
+        self.assertBBQResponse(date(2024, 5, 1), date(2024, 5, 17), "base")
+        self.assertBBQResponse(date(2023, 10, 17), date(2024, 5, 17), "eternity")
+
+        self.assertBBQResponse(date(2024, 9, 20), date(2024, 9, 20), "today")
+        self.assertBBQResponse(date(2024, 9, 19), date(2024, 9, 20), "tomorrow")
+        self.assertBBQResponse(date(2024, 9, 13), date(2024, 9, 20), "week")
+        self.assertBBQResponse(date(2024, 9, 4), date(2024, 9, 20), "base")
