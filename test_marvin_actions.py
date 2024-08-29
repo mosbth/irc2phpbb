f4b91c49ebd64b837047e5b2c2b72f0fe5715189
Daniel Persson <daniel@silvertejp.org>
2024-08-29 16:36:59 +0200

    Add unittest for Strip action

--- test_marvin_actions.py
+++ test_marvin_actions.py
@@ -141,3 +141,18 @@ class ActionTest(unittest.TestCase):
             self.assertActionOutput(
                 marvin_actions.marvinLunch, "dags att luncha", "Jag är lite sugen på Indiska?")
         self.assertActionSilent(marvin_actions.marvinLunch, "matdags")
+
+    def testStrip(self):
+        """Test that marvin can recommend comics"""
+        messageFormat = self.strings.get("commitstrip").get("message")
+        expected = messageFormat.format(url=self.strings.get("commitstrip").get("url"))
+        self.assertActionOutput(marvin_actions.marvinStrip, "lite strip kanske?", expected)
+        self.assertActionSilent(marvin_actions.marvinStrip, "nostrip")
+
+    def testRandomStrip(self):
+        """Test that marvin can recommend random comics"""
+        messageFormat = self.strings.get("commitstrip").get("message")
+        expected = messageFormat.format(url=self.strings.get("commitstrip").get("urlPage") + "123")
+        with mock.patch("marvin_actions.random") as r:
+            r.randint.return_value = 123
+            self.assertActionOutput(marvin_actions.marvinStrip, "random strip kanske?", expected)
